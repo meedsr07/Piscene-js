0fff513969cfcfcf113806dc2c3b1c1770be0cbb
msarar <mohammedsarar7@gmail.com>
2026-04-27 12:28:04 +0100

    execice 7 in js

---
+++ last-first-kiss.js
@@ -0,0 +1,14 @@
+function first(input) {
+return input[0]
+}
+
+function last(input) {
+return input[input.length -1]
+} 
+
+function kiss(input) {
+    let arr = []; 
+    arr.push(input[0])
+    arr.push(input[input.length -1]) 
+    return arr
+}
