66b64caa75ee90f3da5a54689151b7ce2fda2ed6
msarar <mohammedsarar7@gmail.com>
2026-04-27 10:48:28 +0100

    execice 3 in js

---
+++ declarations.js
@@ -0,0 +1,25 @@
+const escapeStr = "`\\/\"'" ;
+
+const arr = [4 ,'2'] ; 
+
+const obj = {
+ str : "" , 
+ num : 0 ,
+ bool : true ,
+ undef : undefined 
+}  
+const nested =  {
+    arr = [4 , undefined , '2'] , 
+    obj =  {
+        str : "" , 
+        num : 0 ,
+        bool : true 
+    }
+}
+
+Object.freeze(arr) 
+Object.freeze(obj)
+Object.freeze(nested) 
+Object.freeze(nested.obj) 
+Object.freeze(nested.arr) 
+
