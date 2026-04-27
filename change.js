3cd598cf527b52d5eb9ac1b463f667b767e761ae
msarar <mohammedsarar7@gmail.com>
2026-04-27 13:03:42 +0100

    exercice 9 in js

---
+++ change.js
@@ -0,0 +1,18 @@
+const sourceObject = {
+  num: 42,
+  bool: true,
+  str: 'some text',
+  log: console.log,
+}
+
+function get(key) {
+return sourceObject[key]
+}
+
+function set(key , value) {
+    return sourceObject[key] = value;
+} 
+
+
+
+
