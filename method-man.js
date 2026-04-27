d6ea78c0f2e199807d6279065d3500d821638188
msarar <mohammedsarar7@gmail.com>
2026-04-27 16:15:19 +0100

    exercice 13 in js

---
+++ method-man.js
@@ -0,0 +1,25 @@
+function words(input ) {
+   let arr = input.split(" ")
+   return arr
+}
+
+function sentence(arr) {
+    let str = arr.join(" ")
+    return str 
+}
+
+function yell(input) {
+    let str = input.toUpperCase()
+    return str 
+}
+
+function whisper(input) {
+    let str = input.toLowerCase()
+    return str
+}
+
+function capitalize(input) {
+let str =  input[0].toUpperCase()+input.slice(1,input.length)
+return str 
+}
+
