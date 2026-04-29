c94a2c31e64321613d410c4aa51456d9d2ef4e79
msarar <mohammedsarar7@gmail.com>
2026-04-29 18:31:17 +0100

    exercie 6 in js q2

---
+++ cut-corners.js
@@ -0,0 +1,66 @@
+function trunc(arr) {
+    
+    let res = [] ;
+    for (let i = 0 ; i < arr.length ; i++) {
+           let nb = arr[i]
+           r = nb - mod(nb ,1)  
+           res.push(r)  
+    }
+    return res
+}
+
+function mod(a, b) {
+  return a - parseInt(a / b) * b;
+}
+
+function round(arr) {
+    let res = [];
+    
+    for (let i = 0 ; i < arr.length ; i++) {
+        let nb  = arr[i] 
+        let sum = 0.5;
+        if (nb < 0 ){
+            sum = -0.5
+        }
+        r = nb + sum
+        res.push(r)
+    }
+    res = trunc(res)
+    return res
+}
+
+
+function floor(arr) {
+    let res = [];
+    for (let i = 0 ; i < arr.length ; i++) {
+        let nb = arr[i] ;
+        
+        if (nb > 0) {
+          nb -=1
+        }
+        res.push(nb)
+
+    }
+    res = trunc(res)
+    return res
+}
+
+
+function ceil(arr) {
+    let res = [];
+    for (let i = 0 ; i < arr.length ; i++) {
+        let nb = arr[i] ;
+        
+        if (nb > 0) {
+          nb +=1
+        }
+        res.push(nb)
+
+    }
+    res = trunc(res)
+    return res
+}
+
+
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(ceil(nums))
