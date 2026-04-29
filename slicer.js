b00af37d15f546ba68c086c0789cf0fe52bbe47f
msarar <mohammedsarar7@gmail.com>
2026-04-29 14:54:00 +0100

    ex 4 in js q2 v4

--- slicer.js
+++ slicer.js
@@ -1,6 +1,13 @@
 function slice(input , start , end = input.length) {
+
     let isArr = Array.isArray(input);
     let res = isArr? [] : "" ;
+    if (start < 0) {
+        start = input.length + start
+    }
+    if (end < 0 ) {
+        end = input.length + end
+    }
     for (let i = start ; i < end ; i++) {
     
         let v = input[i]
