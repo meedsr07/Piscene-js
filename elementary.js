7d46d1452e5e0520ea66bc28634c3c9764a82d38
msarar <mohammedsarar7@gmail.com>
2026-04-29 10:56:03 +0100

    exercice 2 in js q2

---
+++ elementary.js
@@ -0,0 +1,48 @@
+function multiply( a , b) {
+    let res = 0 ;
+    let x = a ;
+    let y = b ;
+    if (a < 0) x = -a ;
+    if  (b < 0) y = -b;
+
+    for (let i = 0 ; i < y ; i++) {
+        res+= x
+    }
+    if ((a < 0 && b >  0) ||(a > 0 && b < 0)) {
+        return -res
+    }
+    return res
+}
+
+function divide(a , b) {
+    let res = 0 ;
+    let x = a ;
+    let y = b ;
+    if (a < 0) x = -a ;
+    if  (b < 0) y = -b; 
+
+    while( x > y) {
+        x-= y
+        res++ 
+    }
+    if ((a < 0 && b > 0)||(a> 0 && b < 0)) {
+        return -res
+    }
+    return res
+} 
+
+function modulo(a , b) {
+    let res = 0 ;
+    let x = a ;
+    let y = b ;
+    if (a < 0) x = -a ;
+    if  (b < 0) y = -b; 
+    while (x >= y) {
+        x -= y 
+        res = x
+    }
+    if ((a < 0 && b > 0)||(a> 0 && b < 0)) {
+        return -res
+    }
+    return res
+}
