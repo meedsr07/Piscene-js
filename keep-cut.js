10c217a4e34e943c25847dd2f174abb9e4aa02fa
msarar <mohammedsarar7@gmail.com>
2026-04-29 15:33:12 +0100

    ex 5 in ks q2

---
+++ keep-cut.js
@@ -0,0 +1,24 @@
+function cutFirst(str) {
+    return str.slice(2)
+}
+function cutLast(str) {
+    return str.slice(0 , str.length -2)
+}
+
+function cutFirstLast(str) {
+    return (cutFirst(cutLast(str)))
+}
+
+function keepFirst(str) {
+    return str.slice(0 ,2)
+}
+function keepLast(str) {
+    return str.slice(str.length -2)
+}
+function keepFirstLast(str) {
+   if (str.length <4) {
+    return str
+   }
+   return keepFirst(str) + keepLast(str)
+   
+}
