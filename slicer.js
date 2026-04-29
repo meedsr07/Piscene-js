4db27091fa6c88905fa4631cbbd8846fedcbe00e
msarar <mohammedsarar7@gmail.com>
2026-04-29 14:44:24 +0100

    ex 4 in js q2 v2

--- slicer.js
+++ slicer.js
@@ -1,4 +1,13 @@
-function slice (input , start , end) {
-    return input.slice(start , end)
+function slice(input , start , end) {
+  
+    let res = Array.isArray(input) ? [] : "" ;
+    for (let i = start ; i < end ; i++) {
+        let v = input[i]
+        if (Array.isArray(input)) {
+            res.push(v)
+        }else{
+            res+= (v)
+        }
+    }
+    return res
 }
-
