0d123767fd868af8f78d4d4092cf37c67173883f
msarar <mohammedsarar7@gmail.com>
2026-04-30 15:14:07 +0100

    ex 12 in js q 2

--- get.js
+++ get.js
@@ -3,6 +3,9 @@ function get(src , path) {
     let c = src ;
     let part = path.split(".") 
     for (let key of part) { 
+        if (c === undefined) {
+            return undefined
+        }
         c = c[key]
     }
     return c
