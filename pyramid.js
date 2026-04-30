b89b03c691f08d591149354b75fb36d5e5ca040d
msarar <mohammedsarar7@gmail.com>
2026-04-30 16:57:52 +0100

    ex13

--- pyramid.js
+++ pyramid.js
@@ -1,20 +1,11 @@
 function pyramid(str, nb) { 
-    let res = [];
+    let res = [] ;
+    let space = " ".repeat(str.length)
 
-    for (let i = 1; i <= nb; i++) {
-        let line = "";
-
-        
-        for (let j = 0; j < nb - i; j++) {
-            line += " ";
-        }
-
-        for (let k = 0; k < (2 * i - 1); k++) {
-            line += str;
-        }
-
-        res.push(line);
+    for (let i = 1; i <=nb;i++ ) {
+        res.push(space.repeat(nb-i)+ str.repeat(2*i-1))
     }
-
-    return res.join("\n");
+    return res.join("\n")
 }
+
+console.log(pyramid("{}" , 12))
