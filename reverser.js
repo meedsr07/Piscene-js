02eddb56b09f2658b1baed247cb193b1b417ced9
msarar <mohammedsarar7@gmail.com>
2026-04-30 11:54:04 +0100

    ex 7 in js q2 v2

--- reverser.js
+++ reverser.js
@@ -1,12 +1,17 @@
 function reverse(input) {
-    let res = Array.isArray(input) ? [] : "" ;
-    for (let i = input.length -1  ; i>= 0 ; i--) {
-        let char = input[i]
-        if (Array.isArray(char)) {
+    let isArr = Array.isArray(input) ;
+    let res =  isArr ? [] : "" ;
+    if (isArr) {
+        for (let i = input.length -1  ; i>= 0 ; i--) {
+            let char = input[i]
             res.push(char)
-        }else {
-            res+= char
         }
+    }else{
+        for (let i = input.length -1  ; i>= 0 ; i--) {
+            let char = input[i]
+            res += char
+        }       
     }
+    
     return res
 }
