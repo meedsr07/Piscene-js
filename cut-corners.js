9b842d48213f8a2deb5d5a3719bbb57dc88c6ec5
msarar <mohammedsarar7@gmail.com>
2026-04-30 10:59:32 +0100

    ex 6 in js q2 v4

--- cut-corners.js
+++ cut-corners.js
@@ -1,81 +1,78 @@
-
-
 function trunc(arr) {
-    let res = [] ;
-    for (let i = 0 ; i < arr.length ; i++) {
-        let isnegative = false ;
-        let nb = arr[i];
-        let step  = 1 ;
+    let res = []; 
+    for ( let i = 0 ; i < arr.length ; i++) {
+        let nb = arr[i]
+        let step = 1 
         let steps = [] ;
+        let isnegative = false ;
         if (nb < 0) {
             nb = nb * -1
             isnegative = true
         }
+
+        // nb = 20 1 2 4 8 16 
         while (step <= nb) {
             steps.push(step)
             step = step * 2
         }
-        let r = 0 ;
-        for (let i = steps.length -1 ; i >= 0 ; i--) {
-            if  (r < nb) {
-                r += steps[i]
+        let resulte = 0 ;
+        for ( let j = steps.length -1 ; j >= 0 ; j--) {
+            if (resulte  + steps[j] <= nb ) {
+                resulte += steps[j]
             }
-            
         }
+        let r = nb -(nb - resulte)
+
         if (isnegative) {
-                r = r * -1
-        
+            r = r * -1
+            isnegative = false
         }
         res.push(r)
     }
     return res
 }
-
 
 function round(arr) {
     let res = [];
     
-    for (let i = 0 ; i < arr.length ; i++) {
-        let nb  = arr[i] 
-        let sum = 0.5;
-        if (nb < 0 ){
-            sum = -0.5
+    for (let i = 0 ; i < arr.length ; i++ ) {
+        let nb = arr[i]
+        let add = 0.5 ;
+        if (nb < 0) {
+            add = -0.5;
         }
-        r = nb + sum
+        let r = nb + add
         res.push(r)
     }
     res = trunc(res)
     return res
 }
 
-
 function floor(arr) {
-    let res = [];
+    let res = [] ;
     for (let i = 0 ; i < arr.length ; i++) {
-        let nb = arr[i] ;
-        
-        if (nb > 0) {
-          nb -=1
+        nb = arr[i]
+        if (nb< 0 ) {
+            nb = nb - 1
         }
         res.push(nb)
-
     }
     res = trunc(res)
-    return res
+    return res 
 }
-
 
 function ceil(arr) {
-    let res = [];
+    let res = [] ;
     for (let i = 0 ; i < arr.length ; i++) {
-        let nb = arr[i] ;
-        
-        if (nb > 0) {
-          nb +=1
+        nb = arr[i]
+        if (nb > 0 ) {
+            nb = nb + 1
         }
         res.push(nb)
-
     }
-    res = trunc(res)
+    res =  trunc(res)
     return res
 }
+
+const nums = [3.7, -3.7, 3.1, -3.1]
+console.log(ceil(nums))
