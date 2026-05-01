0a3749bee34ccf12b50f4108f40fc28cd85ff810
msarar <mohammedsarar7@gmail.com>
2026-05-01 10:32:09 +0100

    ex flat in js

--- flat.js
+++ flat.js
@@ -3,10 +3,16 @@ function flat(arr) {
     for (let i = 0 ; i < arr.length ; i++) {
         let elemnt = arr[i] 
         if (Array.isArray(elemnt)) {
-            res = res.concat(flat(elemnt))
+           for (let j = 0; j < elemnt.length; j++) {
+                res.push(elemnt[j]); 
+            }
         }else{
             res.push(elemnt)
         }
     }
     return res
 }
+
+
+const arr = [1, [2, [3]]]
+console.log(flat(arr))
