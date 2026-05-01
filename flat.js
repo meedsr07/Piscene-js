6369293d77603ac25095ddea74cce58ab292ad85
msarar <mohammedsarar7@gmail.com>
2026-05-01 10:37:15 +0100

    flat

--- flat.js
+++ flat.js
@@ -1,18 +1,18 @@
-function flat(arr) {
-    let res = []; 
-    for (let i = 0 ; i < arr.length ; i++) {
-        let elemnt = arr[i] 
-        if (Array.isArray(elemnt)) {
-           for (let j = 0; j < elemnt.length; j++) {
-                res.push(elemnt[j]); 
-            }
-        }else{
-            res.push(elemnt)
+function flat(arr, depth = 1) {
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let elemnt = arr[i];
+
+        if (Array.isArray(elemnt) && depth > 0) {
+            res = res.concat(flat(elemnt, depth - 1));
+        } else {
+            res.push(elemnt);
         }
     }
-    return res
-}
 
+    return res;
+}
 
 const arr = [1, [2, [3]]]
 console.log(flat(arr))
