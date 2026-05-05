5da9055f18d050394514f64127cf324704edd243
msarar <mohammedsarar@gmail.com>
2026-05-05 18:18:25 +0100

    exercie 2 in js q4

--- date-is.js
+++ date-is.js
@@ -1,3 +1,52 @@
-// function isValid(date) {
-//    return date instanceof Date && 
-// } 
+function isValid(date) {
+    // check if the date is intanceof of Date or is a Number
+  if ( date instanceof Date || typeof date === 'number') {
+    // if hi is not nan 
+    return !isNaN(new Date(date).getTime())
+  }
+  return false
+} 
+
+
+function isAfter(date1 , date2) {
+    if (isValid(date1) && isValid(date2)) {
+        if (date1 > date2) {
+            return true
+        }
+    }
+    return false 
+}
+
+function isBefore(date1 , date2) {
+  if (isValid(date1) && isValid(date2)) {
+    if (date2 > date1) {
+        return true
+    }
+
+  }
+  return false
+}
+
+   
+
+function isFuture(date) {
+    
+    if (isValid(date)) {
+        if (date > Date.now()) {
+            return true
+        }
+    }
+    return false
+}
+
+
+function isPast(date) {
+
+    if (isValid(date)) {
+        if (date < Date.now()) {
+            return true
+        }
+    }
+    return false
+}
+
