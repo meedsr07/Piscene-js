dc8c67542843e40fc3954ffce0ae5d4ce473b388
msarar <mohammmedsarar7@gmail.com>
2026-05-07 17:31:42 +0100

    ex11 in js Q5

---
+++ has-city.js
@@ -0,0 +1,10 @@
+function hasCity(country , cities) {
+ return function (city) {
+    if (cities.includes(city)) {
+        return city + " is a city from " + country 
+    }else {
+        return city + " is not a city from " + country
+    }
+ }   
+}
+
