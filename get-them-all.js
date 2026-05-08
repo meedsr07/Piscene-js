8f3324438eb2f05d9d185338f434105944ef6984
msarar <mohammmedsarar7@gmail.com>
2026-05-08 13:48:19 +0100

    ex1 in Js Q6 v2

--- get-them-all.js
+++ get-them-all.js
@@ -1,12 +1,12 @@
 export function getArchitects() {
     const architects = Array.from(document.getElementsByTagName('a')) 
-    const nonarchitects = Array.from(document.getElementsByTagName('a:not')) 
+    const nonarchitects = Array.from(document.getElementsByTagName('span')) 
     return [architects , nonarchitects] 
 }
 
 export function getClassical() {
-    const classical = Array.from(document.getElementsByClassName('a.classical'))
-    const nonclassical = Array.from(document.getElementsByClassName('a:not.classical') )
+    const classical = Array.from(document.querySelectorAll('a.classical'))
+    const nonclassical = Array.from(document.querySelectorAll('a:not(.classical)') )
     return [classical , nonclassical]
 }
 
