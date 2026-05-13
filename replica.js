baaeddcea3fde4f56c873c2146e732cd9d3d26b4
msarar <mohammmedsarar7@gmail.com>
2026-05-13 18:54:04 +0100

    ex 9 in js Q7 v2

--- replica.js
+++ replica.js
@@ -1,22 +1,22 @@
 function replica(target, ...obj) {
-    for (let i = 0; i < obj.length; i++) {
-        let object = obj[i];
-
-        for (let key of Object.keys(object)) {
-            let value = object[key];
-
-
-            if (typeof value === "object" && !Array.isArray(value)) {
-                if (typeof target[key] !== "object") {
-                    target[key] = {};
-                }
-                replica(target[key], value);
-            } 
-            else {
-                target[key] = value;
-            }
+  for (let i = 0; i < obj.length; i++) {
+    let keys = Object.keys(obj[i]);
+    for (let elment of keys) {
+      let value = obj[i][elment];
+      if (typeof value != 'object' || value instanceof RegExp) {
+        target[elment] = value;
+      } else if (Array.isArray(value)) {
+        target[elment] = value;
+      } else {
+        if (
+          typeof target[elment] !== 'object' ||
+          Array.isArray(target[elment])
+        ) {
+          target[elment] = {};
         }
+        replica(target[elment], value);
+      }
     }
-
-    return target;
+  }
+  return target;
 }
