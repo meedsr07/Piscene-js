b619532c8503d38b326355ccf4680b2a92534859
msarar <mohammmedsarar7@gmail.com>
2026-05-13 18:02:05 +0100

    ex 7 in js Q7

--- curry-entries.js
+++ curry-entries.js
@@ -43,15 +43,6 @@ function reduceCurry( Fn , acc ) {
   }
 }
 
-const personnel = {
-  lukeSkywalker: { id: 5,  pilotingScore: 98, shootingScore: 56, isForceUser: true  },
-  sabineWren:    { id: 82, pilotingScore: 73, shootingScore: 99, isForceUser: false },
-  zebOrellios:   { id: 22, pilotingScore: 20, shootingScore: 59, isForceUser: false },
-  ezraBridger:   { id: 15, pilotingScore: 43, shootingScore: 67, isForceUser: true  },
-  calebDume:     { id: 11, pilotingScore: 71, shootingScore: 85, isForceUser: true  },
-}
-
-
 function filterCurry(Fn) { 
   return  function(obj) {
     let newObj = {} 
