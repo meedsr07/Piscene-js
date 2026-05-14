3cdde8e35fc95921298e96407284ff4052867082
msarar <mohammmedsarar7@gmail.com>
2026-05-14 12:58:01 +0100

    js

--- replica.js
+++ replica.js
@@ -1,22 +1,19 @@
-function replica(target, ...obj) {
-  for (let i = 0; i < obj.length; i++) {
-    let keys = Object.keys(obj[i]);
-    for (let elment of keys) {
-      let value = obj[i][elment];
-      if (typeof value != 'object' || value instanceof RegExp) {
-        target[elment] = value;
-      } else if (Array.isArray(value)) {
-        target[elment] = value;
-      } else {
-        if (
-          typeof target[elment] !== 'object' ||
-          Array.isArray(target[elment])
-        ) {
-          target[elment] = {};
+function replica(target, ...arg ) {
+    for (let i = 0 ; i < arg.length ; i++ ) {
+      let obj = arg[i] 
+      let keys = Object.keys(obj) 
+      for (let key of keys) {
+        let value = obj[key] 
+        if (typeof value  != 'object' || value instanceof RegExp || Array.isArray(value)) {
+          target[key] = value ; 
+        }else {
+              if ( typeof target[key] !=  'object' || target[key] === null || Array.isArray(target[key])) {
+                target[key] = {}
+              }
+              replica(target[key], value)
         }
-        replica(target[elment], value);
       }
     }
-  }
-  return target;
+    return target
 }
+
